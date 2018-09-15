181f5fb49bb8a2e4b957e91822bdbf790345abb8
axenco <AXEL_ENRIQUE_cORU@HOTMAIL.COM>
2018-09-14 20:07:11 -0600

    ok
    ok

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -17,6 +17,47 @@
 
   <body  >  
       <?php   include('../menu/menu.php'); ?>
+
+      <div class="container mt-5"> 
+          <div class="form-row">
+            <div class="col-md-2 col-2">
+              <label for="validationCustom01">Id</label>
+              <input type="number" min="1" class="form-control" id="validationCustom01" placeholder="First name" value="0" required>
+              
+            </div>
+            <div class="col-md-10 col-10 ">
+              <label for="validationCustom02">Nombre</label>
+              <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+               
+            </div> 
+            <div class="col-md-4 col-6 ">
+              <label for="validationCustom02">Apellido Paterno</label>
+              <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+              
+            </div> 
+            <div class="col-md-4 col-6 ">
+              <label for="validationCustom02">Apellido Materno</label>
+              <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+       
+            </div> 
+            <div class="col-md-4 col-12 ">
+              <label for="validationCustom02">Fecha Nacimiento</label>
+              <input type="date" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
+       
+            </div> 
+            <div class="col-md-4 col-12 ">
+               <label for="validationCustom02">Cartera</label>
+              <select class="custom-select">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div> 
+          </div> 
+ 
+      </div>  
+
     
     
   </body>
